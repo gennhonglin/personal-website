8eae97a5fca53ba309e5297f4f3bb2dafad2e3d3
gennhonglin <73550732+gennhonglin@users.noreply.github.com>
2023-12-23 01:59:03 -0800

    adding a footer folder and files and started to develop my footer section of my website

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import "./Footer.scss";
+
+function Footer() {
+    return(
+        <footer className="footer">
+            <p className="footer__copyright">&#169; Genn-Hong Lin</p>
+            <p className="footer__detail">Website designed with enthusiam and late night coding sessions</p>
+        </footer>
+    );
+}
+
+export default Footer;
