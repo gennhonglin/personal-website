8419781aa8f356a45edf97399e1dde52e49e0491
gennhonglin <73550732+gennhonglin@users.noreply.github.com>
2023-12-23 01:45:07 -0800

    developed the contact me section. Where users can use the form to send me an email safely using a third party program called emailJS and developed other part of the contact me section as having my email on display and my social media where people can contact me through linkedin, look at my github, and checkout my volleyball tiktok if wanted. I also added new icons for those sites that I am on.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Homepage from './pages/Homepage/Homepage';
 
 function App() {
