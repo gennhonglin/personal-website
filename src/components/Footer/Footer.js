8c55827d8ccc29411070a5cd1b115d1e81a7a5b3
gennhonglin <73550732+gennhonglin@users.noreply.github.com>
2023-12-23 02:06:39 -0800

    Developed the footer section and made it more personable so the website displays my personality and professionalism as well.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return(
         <footer className="footer">
             <p className="footer__copyright">&#169; Genn-Hong Lin</p>
-            <p className="footer__detail">Website designed with enthusiam and late night coding sessions</p>
+            <p className="footer__detail">Website designed with enthusiam and sleep deprivation 😴😄</p>
         </footer>
     );
 }
