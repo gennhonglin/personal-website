02204bee6b6ddd5d02c16761306d57e2e00d6c92
gennhonglin <73550732+gennhonglin@users.noreply.github.com>
2023-12-25 02:26:04 -0800

    Developed the tablet resolution for the header/navigation section. Made changes to font-size for navigation links and Website title text. As well as made logo bigger and navigation bar bigger on resolution change. Also developed it so instead of hiding the navigation links and having a hamburger bar. I made the links display since the screen size is wide enough.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -31,18 +31,28 @@ function Header() {
 
     return(
         <header className="header">
-            <nav className="header__mobile-nav">
-                <div className="header__mobile-nav__left">
-                    <img alt="logo" src={logo} className="header__mobile-nav__left-logo"></img>
-                    <h2 className="header__mobile-nav__left-title">Genn-Hong Lin</h2>
+            <nav className="header__nav">
+                <div className="header__nav__left">
+                    <img alt="logo" src={logo} className="header__nav__left-logo"></img>
+                    <h2 className="header__nav__left-title">Genn-Hong Lin</h2>
                 </div>
-                <div className="header__mobile-nav__right">
-                    <button className={`header__mobile-nav__right__hamburger ${hamburger ? 'active': ''}`} onClick={()=> {updateMenu()}}>
-                        <div className="header__mobile-nav__right__hamburger-bar"></div>
+                <div className="header__nav__right">
+                    <button className={`header__nav__right__hamburger ${hamburger ? 'active': ''}`} onClick={()=> {updateMenu()}}>
+                        <div className="header__nav__right__hamburger-bar"></div>
                     </button>
+
+                    <div className="header__nav__right__tablet-nav">
+                        <ul className="header__nav__right__tablet-nav__list">
+                            <li className="header__nav__right__tablet-nav__list__item"><a className="header__nav__right__tablet-nav__list__item-link" href="#about">About</a></li>
+                            <li className="header__nav__right__tablet-nav__list__item"><a className="header__nav__right__tablet-nav__list__item-link" href="#project">Project</a></li>
+                            <li className="header__nav__right__tablet-nav__list__item"><a className="header__nav__right__tablet-nav__list__item-link" href="#contact">Contact</a></li>
+                            <li className="header__nav__right__tablet-nav__list__item"><a className="header__nav__right__tablet-nav__list__item-link" href={resume}>Resume</a></li>
+                        </ul>
+                    </div>
+
                 </div>
 
-                <motion.div className={`header__mobile-nav__container ${hamburger ? 'active' : ''}`}
+                <motion.div className={`header__nav__container ${hamburger ? 'active' : ''}`}
                  initial={false}
                  animate={hamburger ? "open" : "closed"}
 
@@ -67,7 +77,7 @@ function Header() {
                     }
                 }}
                 >
-                        <motion.ul className="header__mobile-nav__container__menu"
+                        <motion.ul className="header__nav__container__menu"
                             variants={{
                                 open: {
                                     clipPath: "inset(0% 0% 0% 0% round 10px)",
@@ -89,10 +99,10 @@ function Header() {
                                 }
                             }}
                         >
-                            <motion.li className="header__mobile-nav__container__menu__list" variants={links}><a href="#about" className="header__mobile-nav__container__menu__list-link">About</a></motion.li>
-                            <motion.li className="header__mobile-nav__container__menu__list" variants={links}><a href="#project" className="header__mobile-nav__container__menu__list-link">Projects</a></motion.li>
-                            <motion.li className="header__mobile-nav__container__menu__list" variants={links}><a href="#contact" className="header__mobile-nav__container__menu__list-link">Contact</a></motion.li>
-                            <motion.li className="header__mobile-nav__container__menu__list" variants={links}><a href={resume}  className="header__mobile-nav__container__menu__list-link">Resume</a></motion.li>
+                            <motion.li className="header__nav__container__menu__list" variants={links}><a href="#about" className="header__nav__container__menu__list-link">About</a></motion.li>
+                            <motion.li className="header__nav__container__menu__list" variants={links}><a href="#project" className="header__nav__container__menu__list-link">Projects</a></motion.li>
+                            <motion.li className="header__nav__container__menu__list" variants={links}><a href="#contact" className="header__nav__container__menu__list-link">Contact</a></motion.li>
+                            <motion.li className="header__nav__container__menu__list" variants={links}><a href={resume} className="header__nav__container__menu__list-link">Resume</a></motion.li>
                         </motion.ul>
                 </motion.div>
             </nav>
